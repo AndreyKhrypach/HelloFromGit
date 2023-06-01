45396e54b4dcaf7798f9ed52de4517c054f8c72a
AndreyKhrypach <pacik78@gmail.com>
2023-06-01 10:45:13 +0300

    add new message hello

--- Src/HelloFromGit.java
+++ Src/HelloFromGit.java
@@ -1,5 +1,6 @@
 public class HelloFromGit {
     public static void main(String[] args) {
         System.out.println("Hello from Git");
+        System.out.println("Hello from Git again");
     }
 }
